5522a7b17d38afd48ad6da1d56ed719c86f5c8c0
YevhenHoina <yevhenhoina@gmail.com>
2020-03-04 07:07:47 +0200

    OMG commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// for (let i = COLS_COUNT; i < )
+let arrayOfCells = [];
+    for (let i = 0; i < (COLS_COUNT * ROWS_COUNT); i++){
+        arrayOfCells.push(document.body.querySelector('#c-' + i))
+    }
+
+
+// window.addEventListener('load', function(){
+//     //diagonal-right
+//     for (let i = 0; i < COLS_COUNT; i++){
+//         arrayOfCells[(COLS_COUNT + 1) * i].classList.add('diagonal-right')
+//     }
+//     //diagonal-left
+//     for (let i = 1; i <= COLS_COUNT; i++){
+//         arrayOfCells[(COLS_COUNT - 1) * i].classList.add('diagonal-left')
+//     }
+// })
+document.body.addEventListener('click', function(){
+    
+})
